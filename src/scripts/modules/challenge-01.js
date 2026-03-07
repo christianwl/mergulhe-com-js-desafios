d06ad98393f77d1e5504cd18184ecce5db58c61b
christianwl <chris2w0social@gmail.com>
2026-03-07 06:45:21 -0300

    refactor: modularize challenge 01

--- src/scripts/modules/challenge-01.js
+++ src/scripts/modules/challenge-01.js
@@ -4,41 +4,51 @@ import {
   collectString,
 } from "../utils/prompt-manager.js";
 
-const idAula = 1; // Altere para o ID da aula que você deseja
-obterDesafiosPorId(idAula).then((desafios) => {
-  exibirDesafios(desafios, idAula);
+const ID_AULA = 1;
+obterDesafiosPorId(ID_AULA).then((desafios) => {
+  exibirDesafios(desafios, ID_AULA);
 });
 
 function showChallenge01() {
-  alert("Boas vindas ao nosso site!");
-  let name = "Lua";
-  let age = 25;
+  let name = "";
+  let age = 0;
 
-  const salesCount = 50;
-  const currentBalance = 1000;
+  showDefaultMessages();
 
-  const errorMesage = "Erro! Preencha todos os campos";
+  name = collectUserData("nome");
 
-  alert(errorMesage);
-
-  alert(
-    `Valores padrão armazenados:\n\nNome: ${name}\nIdade: ${age}\nNúmero de Vendas: ${salesCount}\nSaldo Disponível: ${currentBalance}`,
-  );
-
-  alert("Agora insira novos valores para o (Nome) e (Idade)");
-
-  let formattedPrompt = (valueName) =>
-    `Digite s${valueName == "nome" ? "eu" : "ua"} ${valueName}:`;
-
-  name = collectString(formattedPrompt("nome"));
-
-  age = collectPositiveValue(formattedPrompt("idade"));
+  age = collectUserData("idade")
 
   alert(`Valores armazenados:\n\nNome: ${name}\nIdade: ${age}`);
 
   if (age >= 18) {
     alert("Pode tirar a habilitação!");
   }
+}
+
+function showDefaultMessages({name = "Lua", age = 25}){
+  const salesCount = 50;
+  const currentBalance = 1000;
+  const errorMessage = "Erro! Preencha todos os campos";
+
+  alert("Boas vindas ao nosso site!");
+  
+  alert(errorMessage);
+
+  alert(
+    `Valores padrão armazenados:\n\nNome: ${name}\nIdade: ${age}\nNúmero de Vendas: ${salesCount}\nSaldo Disponível: ${currentBalance}`,
+  );
+
+  alert("Agora insira novos valores para o (Nome) e (Idade)");
+}
+
+function collectUserData(valueName){
+  const isName = valueName == "nome";
+  const promptText = `Digite s${isName ? "eu" : "ua"} ${valueName}:`;
+  if(isName){
+    return collectString(promptText);
+  }
+  return collectPositiveValue(promptText);
 }
 
 document.getElementById("iniciar").addEventListener("click", function () {
